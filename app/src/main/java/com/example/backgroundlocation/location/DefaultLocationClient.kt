17d0d6f3d789d728de12fdf8e2a2a5b7fa216e58
cbarrios <lalobarrios16@yahoo.com>
2022-09-28 11:53:33 -0400

    master: (3) location client implementation

---
+++ app/src/main/java/com/example/backgroundlocation/location/DefaultLocationClient.kt
@@ -0,0 +1,68 @@
+package com.example.backgroundlocation.location
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationManager
+import android.os.Looper
+import com.example.backgroundlocation.util.hasLocationPermission
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+
+class DefaultLocationClient(
+    private val context: Context,
+    private val client: FusedLocationProviderClient
+) : LocationClient {
+
+    /**
+     * Throws LocationClient.LocationException when location permissions are not granted
+     * or when location providers cannot be accessed.
+     */
+    @SuppressLint("MissingPermission")
+    override fun getLocationUpdates(interval: Long): Flow<Location> {
+        return callbackFlow {
+            if (!context.hasLocationPermission()) {
+                throw LocationClient.LocationException("Missing location permissions")
+            }
+
+            val locationManager =
+                context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            val isNetworkEnabled =
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+            if (!isGpsEnabled && !isNetworkEnabled) {
+                throw LocationClient.LocationException("Missing location providers")
+            }
+
+            val request = LocationRequest.create().apply {
+                setInterval(interval)
+                fastestInterval = interval
+            }
+
+            val locationCallback = object : LocationCallback() {
+                override fun onLocationResult(result: LocationResult) {
+                    super.onLocationResult(result)
+                    result.locations.lastOrNull()?.let { lastLocation ->
+                        launch { send(lastLocation) }
+                    }
+                }
+            }
+
+            client.requestLocationUpdates(
+                request,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+
+            awaitClose {
+                client.removeLocationUpdates(locationCallback)
+            }
+        }
+    }
+}
